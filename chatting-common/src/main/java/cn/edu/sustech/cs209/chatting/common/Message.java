c097d8e85541e6b3163f9631d5b6e3fc836b99d5
yqxzy <84971201+yqxzy@users.noreply.github.com>
2023-04-25 23:44:56 +0800

    Add files via upload

--- chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Message.java
+++ chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Message.java
@@ -10,11 +10,24 @@ public class Message {
 
     private String data;
 
+    private String name;
+
+
+
+    public Message(Long timestamp, String sentBy, String sendTo, String data,String name) {
+        this.timestamp = timestamp;
+        this.sentBy = sentBy;
+        this.sendTo = sendTo;
+        this.data = data;
+        this.name=name;
+    }
+
     public Message(Long timestamp, String sentBy, String sendTo, String data) {
         this.timestamp = timestamp;
         this.sentBy = sentBy;
         this.sendTo = sendTo;
         this.data = data;
+        this.name=null;
     }
 
     public Long getTimestamp() {
@@ -31,5 +44,8 @@ public class Message {
 
     public String getData() {
         return data;
+    }
+    public String getName() {
+        return name;
     }
 }
